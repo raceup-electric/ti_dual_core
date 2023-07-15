2de48101514a4ee86a40d02f83723f15522a2f78
Davide Frasson <davide01.frasson@gmail.com>
2023-07-15 11:57:46 +0200

    Acceleration Cuman

--- dual_core_completo/dual_core/cpu2_control_unit_test/driver_input.h
+++ dual_core_completo/dual_core/cpu2_control_unit_test/driver_input.h
@@ -17,12 +17,12 @@
  */
 
 // acc1
-#define ACC1_LOW_TH_INITIAL    2800
+#define ACC1_LOW_TH_INITIAL    2530
 #define ACC1_HIGH_TH_INITIAL   3900
 
 // acc2
-#define ACC2_LOW_TH_INITIAL    2100
-#define ACC2_HIGH_TH_INITIAL   3150
+#define ACC2_LOW_TH_INITIAL    1650
+#define ACC2_HIGH_TH_INITIAL   3200
 
 // acc
 #define ACC1_DISC_THRES 100  //ABSOLUTE VAL
@@ -30,8 +30,8 @@
 #define ACC_IMPL_THRES 10 // implausibility threshold 10%
 
 // brake
-#define BRK_LOW_TH_INITIAL    2200
-#define BRK_HIGH_TH_INITIAL   2580
+#define BRK_LOW_TH_INITIAL    2150
+#define BRK_HIGH_TH_INITIAL   2700
 
 #define BRK_DISC_THRES 200  //ABSOLUTE VAL
 
