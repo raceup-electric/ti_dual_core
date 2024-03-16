0671210d68d4f998c6408b9a415b83a2c6b847e0
Alberto Damo <alberto.damo@gmail.com>
2024-03-16 14:15:40 +0100

    remove useless functions atc

--- dual_core_completo/dual_core/cpu2_control_unit_test/atc_management.h
+++ dual_core_completo/dual_core/cpu2_control_unit_test/atc_management.h
@@ -13,21 +13,6 @@ extern float suspensions[4];
 extern float temperatures[10];
 //--------------------------------------
 
-// steering -180 + 180
-unsigned int atc_steering_sensor();
-// acceleration 0-100
-unsigned char atc_acceleration_pedal();
-// brake 0-100
-unsigned char atc_brake_pedal();
-// suspension 200
-unsigned int atc_front_suspension_left();
-
-unsigned int atc_front_suspension_right();
-// temperature 0-150
-unsigned int atc_motor_temperature_left();
-
-unsigned int atc_motor_temperature_right();
-
 void atc_update(unsigned int data[], enum type_message t);
 
 #endif
