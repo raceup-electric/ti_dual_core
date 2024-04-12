e9aa47b0c350aad6bc42184043aa2a6513dc1335
Tommaso Prandin <tommaso.prandin@raceup.it>
2024-04-12 13:58:16 +0200

    Fixed wrong comment in timer setup

--- dual_core_completo/dual_core/cpu2_control_unit_test/timer_management.c
+++ dual_core_completo/dual_core/cpu2_control_unit_test/timer_management.c
@@ -19,8 +19,8 @@ void timerSetup()
        InitCpuTimers();   // For this example, only initialize the Cpu Timers
 
     //
-    // Configure CPU-Timer 0 to __interrupt every 500 milliseconds:
-    // 60MHz CPU Freq, 50 millisecond Period (in uSeconds)
+    // Configure CPU-Timer 0 to __interrupt every m milliseconds:
+    // 200MHz CPU Freq, millisecond Period (in uSeconds)
     //
        ConfigCpuTimer(&CpuTimer0, 200, 10000);
        ConfigCpuTimer(&CpuTimer1, 200, 10000);
