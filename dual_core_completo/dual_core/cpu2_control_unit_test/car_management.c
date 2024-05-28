0407e11bf0488e37345dbb242be9e0d1f8477099
ivanbrillo <brilloivan2@gmail.com>
2024-05-28 10:03:07 +0200

    change initialization max regen current

--- dual_core_completo/dual_core/cpu2_control_unit_test/car_management.c
+++ dual_core_completo/dual_core/cpu2_control_unit_test/car_management.c
@@ -33,7 +33,7 @@ void read_map_sw_message(Uint16 val[])
 
     car_settings.power_limit = presets_power[power_index];
     car_settings.regen_current_scale = presets_regen[regen_index];
-    car_settings.max_regen_current = car_settings.regen_current_scale;
+    car_settings.max_regen_current = PEAK_REGEN_CURRENT * car_settings.regen_current_scale;
 
     car_settings.rear_motor_scale = presets_repartition[(repartition_index)*2];
     car_settings.front_motor_scale = presets_repartition[(repartition_index)*2 +1];
